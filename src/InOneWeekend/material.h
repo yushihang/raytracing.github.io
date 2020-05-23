64fb63ea28ac8c1998594c9943d20ca9a7226c5a
Steve Hollasch <steve@hollasch.net>
2020-05-22 18:55:07 -0700

    img-1.16

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -7,6 +7,13 @@
 struct hit_record;
 
 
+double schlick(double cosine, double ref_idx) {
+    auto r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1 - cosine),5);
+}
+
+
 class material {
     public:
         virtual bool scatter(
@@ -65,7 +72,16 @@ class dielectric : public material {
             vec3 unit_direction = unit_vector(r_in.direction());
             double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
             double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
             if (etai_over_etat * sin_theta > 1.0 ) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+                return true;
+            }
+
+            double reflect_prob = schlick(cos_theta, etai_over_etat);
+            if (random_double() < reflect_prob)
+            {
                 vec3 reflected = reflect(unit_direction, rec.normal);
                 scattered = ray(rec.p, reflected);
                 return true;
