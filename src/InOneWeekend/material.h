6be75f5b94b433bbe802a3741d1db0fe5d95626a
Steve Hollasch <steve@hollasch.net>
2020-05-22 18:30:57 -0700

    img-1.15

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -60,19 +60,23 @@ class dielectric : public material {
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const {
             attenuation = color(1.0, 1.0, 1.0);
-            double etai_over_etat;
-            if (rec.front_face) {
-                etai_over_etat = 1.0 / ref_idx;
-            } else {
-                etai_over_etat = ref_idx;
-            }
+            double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx;
 
             vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+            if (etai_over_etat * sin_theta > 1.0 ) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+                return true;
+            }
+
             vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
             scattered = ray(rec.p, refracted);
             return true;
         }
 
+    public:
         double ref_idx;
 };
 
