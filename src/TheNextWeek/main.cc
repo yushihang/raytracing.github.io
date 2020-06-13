1491d4b5a8a0b5d3ea149a7a237fdc4e70806716
Steve Hollasch <steve@hollasch.net>
2020-06-12 21:03:16 -0700

    img-2.03

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -80,6 +80,18 @@ hittable_list random_scene() {
 }
 
 
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(point3(0,-10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
+
+
 int main() {
     // Image
 
@@ -91,17 +103,40 @@ int main() {
 
     // World
 
-    auto world = random_scene();
+    hittable_list world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+    color background(0,0,0);
+
+    switch (0) {
+        case 1:
+            world = random_scene();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            aperture = 0.1;
+            background = color(0.70, 0.80, 1.00);
+            break;
+
+        default:
+        case 2:
+            world = two_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            background = color(0.70, 0.80, 1.00);
+            break;
+    }
 
     // Camera
 
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
 
