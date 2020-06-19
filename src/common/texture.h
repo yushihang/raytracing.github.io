533702e29594f2628a1bcf175f8060e5c44de640
Trevor David Black <trevordavidblack@gmail.com>
2020-06-18 23:41:48 -0700

    img-2.12

--- src/common/texture.h
+++ src/common/texture.h
@@ -35,7 +35,7 @@ class noise_texture : public texture {
         noise_texture(double sc) : scale(sc) {}
 
         virtual color value(double u, double v, const point3& p) const {
-            return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
+            return color(1,1,1) * noise.turb(scale * p);
         }
 
     public:
