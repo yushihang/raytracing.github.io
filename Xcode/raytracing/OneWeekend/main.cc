7e555d2bb5a9cd81b818139b96e3f25abc072ebf
yushihang <yushihang@gmail.com>
2020-10-30 16:28:59 +0800

    q q q
    q q

--- Xcode/raytracing/OneWeekend/main.cc
+++ Xcode/raytracing/OneWeekend/main.cc
@@ -47,7 +47,7 @@ int main(int argv, char **argc)
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1000;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 1000;
+    const int samples_per_pixel = 10000;
     
     // World
     hittable_list world;
